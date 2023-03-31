d711ee29099c8d48192e4ba53a9c56240445c3f9
Brad Leege <brad.leege@thetradedesk.com>
2023-03-31 12:10:15 -0500

    Initial Port Of Adapter

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UID2GMAPlugin",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "UID2GMAPlugin",
+            targets: ["UID2GMAPlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/IABTechLab/uid2-ios-sdk.git", from: "0.0.3"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", "9.0.0"..<"11.0.0")
+    ],
+    targets: [
+        .target(
+            name: "UID2GMAPlugin",
+            dependencies: [
+                .product(name: "UID2", package: "uid2-ios-sdk"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+            ]),
+        .testTarget(
+            name: "UID2GMAPluginTests",
+            dependencies: ["UID2GMAPlugin"],
+            resources: [
+                .copy("TestData")
+            ])
+    ]
+)
---
+++ Sources/UID2GMAPlugin/UID2GMAMediationAdapter.swift
@@ -0,0 +1,52 @@
+//
+//  UID2GMAMediationAdapter.swift
+//  
+//
+//  Created by Brad Leege on 3/20/23.
+//
+
+import Foundation
+import GoogleMobileAds
+import UID2
+
+/// Adapter to connect UID2 to Google Mobile Ads
+/// https://developers.google.com/admob/ios/open-bidding-adapter
+class UID2GMAMediationAdapter: NSObject {
+    
+    required override init() { }
+    
+}
+
+extension UID2GMAMediationAdapter: GADRTBAdapter {
+
+    func collectSignals(for params: GADRTBRequestParameters, completionHandler: @escaping GADRTBSignalCompletionHandler) {
+        Task {
+            guard let advertisingToken = await UID2Manager.shared.getAdvertisingToken() else {
+                completionHandler(nil, UID2GoogleAdapterErrors.advertisingTokenNotFoundForGMA)
+                return
+            }
+            completionHandler(advertisingToken, nil)
+        }
+    }
+    
+    static func adapterVersion() -> GADVersionNumber {
+        var version = GADVersionNumber()
+        version.majorVersion = 0
+        version.minorVersion = 0
+        version.patchVersion = 1
+        return version
+    }
+    
+    static func adSDKVersion() -> GADVersionNumber {
+        var version = GADVersionNumber()
+        version.majorVersion = UID2SDKProperties.getUID2SDKVersion().major
+        version.minorVersion = UID2SDKProperties.getUID2SDKVersion().minor
+        version.patchVersion = UID2SDKProperties.getUID2SDKVersion().patch
+        return version
+    }
+    
+    static func networkExtrasClass() -> GADAdNetworkExtras.Type? {
+        return nil
+    }
+    
+}
