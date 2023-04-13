3a924828e204c4dc1a8e08199e56fab392705c42
Brad Leege <bleege@gmail.com>
2023-04-12 21:07:40 -0500

    New UID2GoogleGMADevelopmentApp

---
+++ Development/UID2GoogleGMADevelopmentApp/UID2GoogleGMADevelopmentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UID2GoogleGMADevelopmentApp
+//
+//  Created by Brad Leege on 4/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
