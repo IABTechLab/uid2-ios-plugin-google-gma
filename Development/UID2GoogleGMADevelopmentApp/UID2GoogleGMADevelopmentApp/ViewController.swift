3ab623b3816e0435e1a1ccdb4caa20c1a390d6b0
Brad Leege <brad.leege@thetradedesk.com>
2023-04-13 09:50:52 -0500

    Loading UID2 Identity Into GMA DevApp

--- Development/UID2GoogleGMADevelopmentApp/UID2GoogleGMADevelopmentApp/ViewController.swift
+++ Development/UID2GoogleGMADevelopmentApp/UID2GoogleGMADevelopmentApp/ViewController.swift
@@ -9,6 +9,7 @@
 
 import GoogleMobileAds
 import UIKit
+import UID2
 
 class ViewController: UIViewController {
 
@@ -17,10 +18,42 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Google Mobile Ads SDK version: \(GADMobileAds.sharedInstance().sdkVersion)")
-        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
-        bannerView.rootViewController = self
-        bannerView.load(GADRequest())
+        Task {
+            await loadUID2Identity()
+            print("Google Mobile Ads SDK version: \(GADMobileAds.sharedInstance().sdkVersion)")
+            bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+            bannerView.rootViewController = self
+            bannerView.load(GADRequest())
+        }
+    }
+ 
+    private func loadUID2Identity() async {
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            // Sample UID2Identity data
+            let uid2IdentityData = try AppDataLoader.load(fileName: "uid2identity", fileExtension: "json")
+            let uid2IdentityFromFile = try decoder.decode(UID2Identity.self, from: uid2IdentityData)
+            
+            // Emulate A UID2Identity With Valid Times
+            let identityExpires = Date(timeIntervalSinceNow: 60 * 60).millisecondsSince1970
+            let refreshFrom = Date(timeIntervalSinceNow: 60 * 40).millisecondsSince1970
+            let refreshExpires = Date(timeIntervalSinceNow: 60 * 50).millisecondsSince1970
+            
+            let uid2Identity = UID2Identity(advertisingToken: uid2IdentityFromFile.advertisingToken,
+                                            refreshToken: uid2IdentityFromFile.refreshToken,
+                                            identityExpires: identityExpires,
+                                            refreshFrom: refreshFrom,
+                                            refreshExpires: refreshExpires,
+                                            refreshResponseKey: uid2IdentityFromFile.refreshResponseKey)
+            
+            await UID2Manager.shared.setIdentity(uid2Identity)
+        } catch {
+            print("Error loading UID2Identity")
+        }
+        
     }
     
 }
