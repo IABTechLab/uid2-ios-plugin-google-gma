476143eaa57b92db92335e74a9865bbbd37c46ad
Brad Leege <bleege@gmail.com>
2023-04-12 21:51:36 -0500

    Bringing in BaseExample Code

--- Development/UID2GoogleGMADevelopmentApp/UID2GoogleGMADevelopmentApp/ViewController.swift
+++ Development/UID2GoogleGMADevelopmentApp/UID2GoogleGMADevelopmentApp/ViewController.swift
@@ -2,18 +2,25 @@
 //  ViewController.swift
 //  UID2GoogleGMADevelopmentApp
 //
+//  See: https://github.com/googleads/googleads-mobile-ios-examples/tree/main/Swift/admob/BannerExample
+//
 //  Created by Brad Leege on 4/12/23.
 //
 
+import GoogleMobileAds
 import UIKit
 
 class ViewController: UIViewController {
 
+    /// The banner view.
+    @IBOutlet weak var bannerView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Google Mobile Ads SDK version: \(GADMobileAds.sharedInstance().sdkVersion)")
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
     }
-
-
+    
 }
-
